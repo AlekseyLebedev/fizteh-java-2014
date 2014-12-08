5ca9f54190a7044f720dc79b9326eb1f98cb7eb0
Lebedev Aleksey <leb-al@outlook.com>
2014-12-08 15:15:45 +0300

    JUnit v1.3 (add final to variable)ш

--- src/ru/fizteh/fivt/students/LebedevAleksey/junit/Table.java
+++ src/ru/fizteh/fivt/students/LebedevAleksey/junit/Table.java
@@ -96,7 +96,7 @@ public class Table extends ru.fizteh.fivt.students.LebedevAleksey.MultiFileHashM
                 items.add(key);
             }
         }
-        ArrayList<String> result = new ArrayList<>(items.size());
+        final ArrayList<String> result = new ArrayList<>(items.size());
         items.forEach(new Consumer<String>() {
             @Override
             public void accept(String s) {
@@ -119,7 +119,6 @@ public class Table extends ru.fizteh.fivt.students.LebedevAleksey.MultiFileHashM
     public int rollback() {
         int changes = changesCount();
         changedKeys.clear();
-        initParts();
         return changes;
     }
 }
