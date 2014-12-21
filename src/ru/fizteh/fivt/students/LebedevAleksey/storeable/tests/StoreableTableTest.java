30620d636314626094130483ad4627ff2abaed88
Lebedev Aleksey <leb-al@outlook.com>
2014-12-21 17:28:42 +0300

    Storeable v1.0 (checkstyle)

--- src/ru/fizteh/fivt/students/LebedevAleksey/storeable/tests/StoreableTableTest.java
+++ src/ru/fizteh/fivt/students/LebedevAleksey/storeable/tests/StoreableTableTest.java
@@ -15,7 +15,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 
 public class StoreableTableTest {
     @Rule
